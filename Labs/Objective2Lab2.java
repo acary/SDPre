b00c2c656be705616b01667808ac8dcb520f808c
Andy Cary <info@andycary.com>
2021-12-15 15:14:07 -0500

    complete Objective2Lab3

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Andrew David";
+    int age = 32;
+    String state = "Colorado";
+
+    System.out.println("Name: " + name + " " + "Age: " + age + " " + "State: " + state);
+  }
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 3.84;
+    num2 = 4.00;
+    num3 = 4.63;
+
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
